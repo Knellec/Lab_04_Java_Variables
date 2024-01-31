e444d09c58bb120d8a003fcff078cb793ea60fc1
Knellec <ethancknell@gmail.com>
2024-01-31 11:05:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+/*
+
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        /*   int age = 0;
+           double salary - 0.0;
+           string nme = "Tom"
+           boolean done = false;
+         */
+        int intOperandA = 23;
+        int intOperandB = 17;
+        int intSum = 0;
+        int intProduct = 0;
+        int intQuoutient = 0;
+        int intDifferenct = 0;
+        int intModulo = 0; // remainder uses the %
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+
+
+        intModulo = intOperandA % intOperandB;
+
+        // Put in the rest of the in statements here
+
+        double doubleOperandA = 23;
+        double doubleOperandB = 17;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleQuoutient = 0;
+        double doubleDifferenct = 0;
+        double doubleModulo = 0; // remainder uses the
+    }
+}
