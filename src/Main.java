690f910d0fef70e92abd0b4c6d6fa7e8823a3727
Knellec <ethancknell@gmail.com>
2024-02-07 16:58:56 -0500

    Lab 4

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,67 @@ public class Main
            boolean done = false;
          */
         int intOperandA = 23;
-        int intOperandB = 17;
-        int intSum = 0;
-        int intProduct = 0;
-        int intQuoutient = 0;
-        int intDifferenct = 0;
-        int intModulo = 0; // remainder uses the %
+        int intOperandB = 5;
+        int intSum = 6;
+        int intProduct = 2;
+        int intDifference = 3;
+        int intQuotient = 15;
+        int intModulo = 22; // remainder uses the %
+
+        String name = "TOM";
 
         intSum = intOperandA + intOperandB;
         System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
 
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
 
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The Quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
 
         intModulo = intOperandA % intOperandB;
+        System.out.println("The Modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo);
 
         // Put in the rest of the in statements here
 
         double doubleOperandA = 23;
         double doubleOperandB = 17;
-        double doubleSum = 0;
-        double doubleProduct = 0;
-        double doubleQuoutient = 0;
-        double doubleDifferenct = 0;
+        double doubleSum = 6;
+        double doubleProduct = 2;
+        double doubleDifference = 5;
+        double doubleQuotient = 1;
+
         double doubleModulo = 0; // remainder uses the
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The Quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The Modulo of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+
+
+
+        double myLunchCost = 12.50;
+        int numOfKids = 2;
+        String isRaining = "no";
+        double gasPrice = 2.85;
+        int favNum = 3;
+        double shoeSize = 11.5;
+        int birthMonth = 5;
+        String fullName = "Ethan Knell";
+
+
     }
 }
